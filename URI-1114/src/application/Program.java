a5d45d6f3bec968249723f3e97bc9b8cbe735a42
linco <linco@DESKTOP-VIJS1C2>
2022-04-08 18:14:11 -0300

    Indentation code

--- URI-1114/src/application/Program.java
+++ URI-1114/src/application/Program.java
@@ -6,22 +6,21 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Insira a senha de acesso: ");
-		
+
 		int key = sc.nextInt();
-		
+
 		while (key != 2002) {
 			System.out.println("Acesso negado");
 			System.out.print("Insira a senha de acesso: ");
 			key = sc.nextInt();
 		}
 		System.out.println("Acesso Autorizado");
-		
-		
+
 		sc.close();
 
 	}
